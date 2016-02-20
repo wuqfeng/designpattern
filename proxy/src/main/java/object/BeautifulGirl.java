5417008176fe63a8759e880af6054834a1d7b6e6
deadking01 <wuqingfeng234@163.com>
2016-02-20 19:14:34 +0800

    proxy

---
+++ proxy/src/main/java/object/BeautifulGirl.java
@@ -0,0 +1,12 @@
+package object;
+
+/**
+ * Created by wuqf on 16-2-20.
+ */
+public class BeautifulGirl {
+    public BeautifulGirl(String name) {
+        this.name = name;
+    }
+
+    public String name;
+}
---
+++ proxy/src/main/java/subject/GiveGift.java
@@ -0,0 +1,12 @@
+package subject;
+
+/**
+ * Created by wuqf on 16-2-20.
+ */
+public interface GiveGift {
+    public void giveFlowers();
+
+    public void giveChocolates();
+
+    public void giveBook();
+}
